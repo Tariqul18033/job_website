dcbce6ad3ead7c5804ed078501f130184dcd79c0
Tariqul <tariqul18033@gmail.com>
2025-04-30 16:31:02 +0600

    user

--- resources/views/components/listing-tags.blade.php
+++ resources/views/components/listing-tags.blade.php
@@ -1,14 +1,13 @@
+@props(['tagsCsv'])
+
 @php
-    $tags = explode(',', $listing->tags);
+$tags = explode(',', $tagsCsv);
 @endphp
 
 <ul class="flex">
-
-    @foreach ($tags as $tag)
-    <li
-        class="bg-black text-white rounded-xl px-3 py-1 mr-2"
-    >
-        <a href="/?tags={{$tag}}">{{$tag}}</a>
-    </li>
-    @endforeach
+  @foreach($tags as $tag)
+  <li class="flex items-center justify-center bg-black text-white rounded-xl py-1 px-3 mr-2 text-xs">
+    <a href="/?tag={{$tag}}">{{$tag}}</a>
+  </li>
+  @endforeach
 </ul>
--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,11 @@
 <?php
 
-use Illuminate\Support\Facades\Route;
 use App\Models\Job;
 use App\Models\Listing;
 use Illuminate\Support\Facades\View;
+use Illuminate\Support\Facades\Route;
+
+use App\Http\Controllers\UserController;
 use App\Http\Controllers\ListingController;
 Route::get('/', [ListingController::class, 'index'])->name('home');
 
@@ -16,9 +18,21 @@ Route::get('/listings/create', [ListingController::class, 'create']);
 Route::post('/listings', [ListingController::class, 'store']);
 
 
-Route::get('/listings/{id}', [ListingController::class, 'show'])->name('listing.show');
+Route::get('/listings/{id}', [ListingController::class, 'show']);
 //create
 
 //Show Edit Form
 Route::get('/listings/{listing}/edit', [ListingController::class, 'edit'])->name('listing.edit');
 Route::put('/listings/{listing}', [ListingController::class, 'update']);//->middleware('auth');
+Route::delete('/listings/{listing}', [ListingController::class, 'destroy']);
+
+// Show Register/Create Form
+Route::get('/register', [UserController::class, 'create']);
+// Create New User
+Route::post('/users', [UserController::class, 'store']);
+// Log User Out
+Route::post('/logout', [UserController::class, 'logout'])->middleware('auth');
+// Show Login Form
+Route::get('/login', [UserController::class, 'login'])->name('login')->middleware('guest');
+// Log In User
+Route::post('/users/authenticate', [UserController::class, 'authenticate']);
